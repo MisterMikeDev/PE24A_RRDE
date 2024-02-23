c6726545614e83a22b5794538d907fe09c89ecdd
MisterMikeDev <diegoerr04@gmail.com>
2024-02-22 21:41:04 -0600

    :fire: Avances

--- PE24A_RRDE/PE24A_RRDE/DlgMesaParcticas1.Designer.cs
+++ PE24A_RRDE/PE24A_RRDE/DlgMesaParcticas1.Designer.cs
@@ -32,16 +32,32 @@
             this.label1 = new System.Windows.Forms.Label();
             this.LogoMesa1 = new System.Windows.Forms.PictureBox();
             this.PnlIzquierdo = new System.Windows.Forms.Panel();
+            this.BtnP2Llenar = new System.Windows.Forms.Button();
+            this.BtnP2Activar = new System.Windows.Forms.Button();
+            this.BtnFillTabla = new System.Windows.Forms.Button();
+            this.BtnP1Muestra = new System.Windows.Forms.Button();
+            this.BtnTest = new System.Windows.Forms.Button();
             this.TextBoxNum2 = new System.Windows.Forms.TextBox();
             this.TextBoxNum1 = new System.Windows.Forms.TextBox();
             this.PnlDerecho = new System.Windows.Forms.Panel();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.PnlCentral = new System.Windows.Forms.Panel();
-            this.BtnTest = new System.Windows.Forms.Button();
+            this.DgvTabla1 = new System.Windows.Forms.DataGridView();
+            this.ColNombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColApellidoP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColApellidoM = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LblSalida1 = new System.Windows.Forms.Label();
+            this.TextBoxSalida1 = new System.Windows.Forms.TextBox();
+            this.TextBoxSalida2 = new System.Windows.Forms.TextBox();
+            this.LblSalida2 = new System.Windows.Forms.Label();
+            this.TextBoxSalida3 = new System.Windows.Forms.TextBox();
+            this.LblSalida3 = new System.Windows.Forms.Label();
             this.PnlSuperior.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.LogoMesa1)).BeginInit();
             this.PnlIzquierdo.SuspendLayout();
             this.PnlDerecho.SuspendLayout();
+            this.PnlCentral.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvTabla1)).BeginInit();
             this.SuspendLayout();
             // 
             // PnlSuperior
@@ -54,7 +70,6 @@
             this.PnlSuperior.Name = "PnlSuperior";
             this.PnlSuperior.Size = new System.Drawing.Size(1484, 80);
             this.PnlSuperior.TabIndex = 0;
-            this.PnlSuperior.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // label1
             // 
@@ -66,7 +81,6 @@
             this.label1.Size = new System.Drawing.Size(327, 35);
             this.label1.TabIndex = 1;
             this.label1.Text = "Mesa de trabajo I";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // LogoMesa1
             // 
@@ -77,11 +91,14 @@
             this.LogoMesa1.Size = new System.Drawing.Size(53, 59);
             this.LogoMesa1.TabIndex = 0;
             this.LogoMesa1.TabStop = false;
-            this.LogoMesa1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // PnlIzquierdo
             // 
             this.PnlIzquierdo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(79)))), ((int)(((byte)(79)))));
+            this.PnlIzquierdo.Controls.Add(this.BtnP2Llenar);
+            this.PnlIzquierdo.Controls.Add(this.BtnP2Activar);
+            this.PnlIzquierdo.Controls.Add(this.BtnFillTabla);
+            this.PnlIzquierdo.Controls.Add(this.BtnP1Muestra);
             this.PnlIzquierdo.Controls.Add(this.BtnTest);
             this.PnlIzquierdo.Controls.Add(this.TextBoxNum2);
             this.PnlIzquierdo.Controls.Add(this.TextBoxNum1);
@@ -90,7 +107,56 @@
             this.PnlIzquierdo.Name = "PnlIzquierdo";
             this.PnlIzquierdo.Size = new System.Drawing.Size(150, 728);
             this.PnlIzquierdo.TabIndex = 1;
-            this.PnlIzquierdo.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint_1);
+            // 
+            // BtnP2Llenar
+            // 
+            this.BtnP2Llenar.Location = new System.Drawing.Point(15, 385);
+            this.BtnP2Llenar.Name = "BtnP2Llenar";
+            this.BtnP2Llenar.Size = new System.Drawing.Size(118, 50);
+            this.BtnP2Llenar.TabIndex = 6;
+            this.BtnP2Llenar.Text = "P2 Calcular";
+            this.BtnP2Llenar.UseVisualStyleBackColor = true;
+            this.BtnP2Llenar.Click += new System.EventHandler(this.BtnP2Llenar_Click);
+            // 
+            // BtnP2Activar
+            // 
+            this.BtnP2Activar.Location = new System.Drawing.Point(15, 326);
+            this.BtnP2Activar.Name = "BtnP2Activar";
+            this.BtnP2Activar.Size = new System.Drawing.Size(118, 50);
+            this.BtnP2Activar.TabIndex = 5;
+            this.BtnP2Activar.Text = "P2 Activar";
+            this.BtnP2Activar.UseVisualStyleBackColor = true;
+            this.BtnP2Activar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // BtnFillTabla
+            // 
+            this.BtnFillTabla.Location = new System.Drawing.Point(14, 248);
+            this.BtnFillTabla.Name = "BtnFillTabla";
+            this.BtnFillTabla.Size = new System.Drawing.Size(118, 50);
+            this.BtnFillTabla.TabIndex = 4;
+            this.BtnFillTabla.Text = "P1 Llenar";
+            this.BtnFillTabla.UseVisualStyleBackColor = true;
+            this.BtnFillTabla.Click += new System.EventHandler(this.BtnFillTabla_Click);
+            // 
+            // BtnP1Muestra
+            // 
+            this.BtnP1Muestra.Location = new System.Drawing.Point(14, 189);
+            this.BtnP1Muestra.Name = "BtnP1Muestra";
+            this.BtnP1Muestra.Size = new System.Drawing.Size(118, 50);
+            this.BtnP1Muestra.TabIndex = 3;
+            this.BtnP1Muestra.Text = "P1 Muestra";
+            this.BtnP1Muestra.UseVisualStyleBackColor = true;
+            this.BtnP1Muestra.Click += new System.EventHandler(this.BtnMostrarPractica1_Click);
+            // 
+            // BtnTest
+            // 
+            this.BtnTest.Location = new System.Drawing.Point(13, 119);
+            this.BtnTest.Name = "BtnTest";
+            this.BtnTest.Size = new System.Drawing.Size(118, 50);
+            this.BtnTest.TabIndex = 2;
+            this.BtnTest.Text = "Boton de prueba";
+            this.BtnTest.UseVisualStyleBackColor = true;
+            this.BtnTest.Click += new System.EventHandler(this.BtnTest_Click);
             // 
             // TextBoxNum2
             // 
@@ -98,7 +164,6 @@
             this.TextBoxNum2.Name = "TextBoxNum2";
             this.TextBoxNum2.Size = new System.Drawing.Size(119, 20);
             this.TextBoxNum2.TabIndex = 1;
-            this.TextBoxNum2.TextChanged += new System.EventHandler(this.TextBoxNum2_TextChanged);
             // 
             // TextBoxNum1
             // 
@@ -106,19 +171,23 @@
             this.TextBoxNum1.Name = "TextBoxNum1";
             this.TextBoxNum1.Size = new System.Drawing.Size(119, 20);
             this.TextBoxNum1.TabIndex = 0;
-            this.TextBoxNum1.TextChanged += new System.EventHandler(this.TextBoxNum1_TextChanged);
             // 
             // PnlDerecho
             // 
             this.PnlDerecho.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(79)))), ((int)(((byte)(79)))));
             this.PnlDerecho.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.PnlDerecho.Controls.Add(this.TextBoxSalida3);
+            this.PnlDerecho.Controls.Add(this.LblSalida3);
+            this.PnlDerecho.Controls.Add(this.TextBoxSalida2);
+            this.PnlDerecho.Controls.Add(this.LblSalida2);
+            this.PnlDerecho.Controls.Add(this.TextBoxSalida1);
+            this.PnlDerecho.Controls.Add(this.LblSalida1);
             this.PnlDerecho.Controls.Add(this.comboBox1);
             this.PnlDerecho.Dock = System.Windows.Forms.DockStyle.Right;
             this.PnlDerecho.Location = new System.Drawing.Point(1334, 80);
             this.PnlDerecho.Name = "PnlDerecho";
             this.PnlDerecho.Size = new System.Drawing.Size(150, 728);
             this.PnlDerecho.TabIndex = 2;
-            this.PnlDerecho.Paint += new System.Windows.Forms.PaintEventHandler(this.PnlDerecho_Paint);
             // 
             // comboBox1
             // 
@@ -133,22 +202,93 @@
             this.PnlCentral.BackColor = System.Drawing.Color.Transparent;
             this.PnlCentral.BackgroundImage = global::PE24A_RRDE.Properties.Resources.bg;
             this.PnlCentral.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.PnlCentral.Controls.Add(this.DgvTabla1);
             this.PnlCentral.Dock = System.Windows.Forms.DockStyle.Fill;
             this.PnlCentral.Location = new System.Drawing.Point(150, 80);
             this.PnlCentral.Name = "PnlCentral";
             this.PnlCentral.Size = new System.Drawing.Size(1184, 728);
             this.PnlCentral.TabIndex = 3;
-            this.PnlCentral.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint_2);
             // 
-            // BtnTest
+            // DgvTabla1
             // 
-            this.BtnTest.Location = new System.Drawing.Point(13, 119);
-            this.BtnTest.Name = "BtnTest";
-            this.BtnTest.Size = new System.Drawing.Size(118, 31);
-            this.BtnTest.TabIndex = 2;
-            this.BtnTest.Text = "Boton de prueba";
-            this.BtnTest.UseVisualStyleBackColor = true;
-            this.BtnTest.Click += new System.EventHandler(this.BtnTest_Click);
+            this.DgvTabla1.BackgroundColor = System.Drawing.Color.White;
+            this.DgvTabla1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvTabla1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColNombre,
+            this.ColApellidoP,
+            this.ColApellidoM});
+            this.DgvTabla1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DgvTabla1.Location = new System.Drawing.Point(0, 0);
+            this.DgvTabla1.Name = "DgvTabla1";
+            this.DgvTabla1.Size = new System.Drawing.Size(1184, 728);
+            this.DgvTabla1.TabIndex = 0;
+            this.DgvTabla1.Visible = false;
+            // 
+            // ColNombre
+            // 
+            this.ColNombre.HeaderText = "Nombre";
+            this.ColNombre.Name = "ColNombre";
+            // 
+            // ColApellidoP
+            // 
+            this.ColApellidoP.HeaderText = "Apellido Paterno";
+            this.ColApellidoP.Name = "ColApellidoP";
+            // 
+            // ColApellidoM
+            // 
+            this.ColApellidoM.HeaderText = "Apellido Materno";
+            this.ColApellidoM.Name = "ColApellidoM";
+            // 
+            // LblSalida1
+            // 
+            this.LblSalida1.AutoSize = true;
+            this.LblSalida1.ForeColor = System.Drawing.Color.White;
+            this.LblSalida1.Location = new System.Drawing.Point(18, 82);
+            this.LblSalida1.Name = "LblSalida1";
+            this.LblSalida1.Size = new System.Drawing.Size(45, 13);
+            this.LblSalida1.TabIndex = 1;
+            this.LblSalida1.Text = "Salida 1";
+            // 
+            // TextBoxSalida1
+            // 
+            this.TextBoxSalida1.Location = new System.Drawing.Point(18, 99);
+            this.TextBoxSalida1.Name = "TextBoxSalida1";
+            this.TextBoxSalida1.Size = new System.Drawing.Size(100, 20);
+            this.TextBoxSalida1.TabIndex = 2;
+            // 
+            // TextBoxSalida2
+            // 
+            this.TextBoxSalida2.Location = new System.Drawing.Point(18, 156);
+            this.TextBoxSalida2.Name = "TextBoxSalida2";
+            this.TextBoxSalida2.Size = new System.Drawing.Size(100, 20);
+            this.TextBoxSalida2.TabIndex = 4;
+            // 
+            // LblSalida2
+            // 
+            this.LblSalida2.AutoSize = true;
+            this.LblSalida2.ForeColor = System.Drawing.Color.White;
+            this.LblSalida2.Location = new System.Drawing.Point(18, 139);
+            this.LblSalida2.Name = "LblSalida2";
+            this.LblSalida2.Size = new System.Drawing.Size(45, 13);
+            this.LblSalida2.TabIndex = 3;
+            this.LblSalida2.Text = "Salida 2";
+            // 
+            // TextBoxSalida3
+            // 
+            this.TextBoxSalida3.Location = new System.Drawing.Point(18, 214);
+            this.TextBoxSalida3.Name = "TextBoxSalida3";
+            this.TextBoxSalida3.Size = new System.Drawing.Size(100, 20);
+            this.TextBoxSalida3.TabIndex = 6;
+            // 
+            // LblSalida3
+            // 
+            this.LblSalida3.AutoSize = true;
+            this.LblSalida3.ForeColor = System.Drawing.Color.White;
+            this.LblSalida3.Location = new System.Drawing.Point(18, 197);
+            this.LblSalida3.Name = "LblSalida3";
+            this.LblSalida3.Size = new System.Drawing.Size(45, 13);
+            this.LblSalida3.TabIndex = 5;
+            this.LblSalida3.Text = "Salida 3";
             // 
             // DlgMesaParcticas1
             // 
@@ -161,14 +301,16 @@
             this.Controls.Add(this.PnlSuperior);
             this.Name = "DlgMesaParcticas1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "DlgMesaParcticas1";
-            this.Load += new System.EventHandler(this.DlgMesaParcticas1_Load);
+            this.Text = "Mesa Parcticas 1";
             this.PnlSuperior.ResumeLayout(false);
             this.PnlSuperior.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.LogoMesa1)).EndInit();
             this.PnlIzquierdo.ResumeLayout(false);
             this.PnlIzquierdo.PerformLayout();
             this.PnlDerecho.ResumeLayout(false);
+            this.PnlDerecho.PerformLayout();
+            this.PnlCentral.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DgvTabla1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -185,5 +327,19 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button BtnTest;
+        private System.Windows.Forms.DataGridView DgvTabla1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColNombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColApellidoP;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColApellidoM;
+        private System.Windows.Forms.Button BtnP1Muestra;
+        private System.Windows.Forms.Button BtnFillTabla;
+        private System.Windows.Forms.Button BtnP2Llenar;
+        private System.Windows.Forms.Button BtnP2Activar;
+        private System.Windows.Forms.TextBox TextBoxSalida2;
+        private System.Windows.Forms.Label LblSalida2;
+        private System.Windows.Forms.TextBox TextBoxSalida1;
+        private System.Windows.Forms.Label LblSalida1;
+        private System.Windows.Forms.TextBox TextBoxSalida3;
+        private System.Windows.Forms.Label LblSalida3;
     }
 }
--- PE24A_RRDE/PE24A_RRDE/DlgMesaParcticas1.cs
+++ PE24A_RRDE/PE24A_RRDE/DlgMesaParcticas1.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Drawing;
+using System.Reflection;
+using System.Runtime.ExceptionServices;
 using System.Windows.Forms;
 
 namespace PE24A_RRDE
@@ -68,54 +71,171 @@ namespace PE24A_RRDE
         }
 
         /* ------------------------------------------------------------------------- */
-        // TextBox del numero 1
+        // Muestra la tabla de la practica 1
         /* ------------------------------------------------------------------------- */
-        private void TextBoxNum1_TextChanged(object sender, EventArgs e)
+        private void BtnMostrarPractica1_Click(object sender, EventArgs e)
         {
-
+            DgvTabla1.Visible = !DgvTabla1.Visible;
         }
 
         /* ------------------------------------------------------------------------- */
-        // TextBox del numero 2
+        // Rellena la tabla con elementos de prueba
         /* ------------------------------------------------------------------------- */
-        private void TextBoxNum2_TextChanged(object sender, EventArgs e)
+        private void BtnFillTabla_Click(object sender, EventArgs e)
         {
+            DgvTabla1.Rows.Clear();
 
+            int Index = 4, RowReference = 0;
+
+            for (int x = 0; x < Index; x++)
+            {
+                for (int  y = 0; y < Index; y++)
+                {
+                    for (int z = 0; z < Index; z++)
+                    {
+                        DgvTabla1.Rows.Add();
+                        DgvTabla1.Rows[RowReference].Cells[0].Value = $"{x}";
+                        DgvTabla1.Rows[RowReference].Cells[1].Value = $"{y}";
+                        DgvTabla1.Rows[RowReference].Cells[2].Value = $"{z}";
+                        RowReference++;
+                    }
+
+                }
+            }
         }
 
-        private void DlgMesaParcticas1_Load(object sender, EventArgs e)
-        {
+        /* ------------------------------------------------------------------------- */
+        // Botón de Activar tabla Practica 2
+        /* ------------------------------------------------------------------------- */
+        DataGridView DgvTabla2;
+        private void button2_Click(object sender, EventArgs e)
+{
+            int numColums;
+            bool isNumber;
+            Random random = new Random();
 
+            /* ------------------------------------------------------------------------- */
+            // Construye el componente
+            /* ------------------------------------------------------------------------- */
+            DgvTabla2 = new DataGridView();
+            PnlCentral.Controls.Add(DgvTabla2);
+
+            /* ------------------------------------------------------------------------- */
+            // Validaciones
+            /* ------------------------------------------------------------------------- */
+            isNumber = int.TryParse(TextBoxNum1.Text, out numColums);
+
+            if (!isNumber)
+            {
+                MessageBox.Show("Debes capturar un número de columns");
+                TextBoxNum1.Focus();
+                return;
+            }
+
+            /* ------------------------------------------------------------------------- */
+            // Genera la configuración basica del componente
+            /* ------------------------------------------------------------------------- */
+            DgvTabla2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            DgvTabla2.Dock = DockStyle.Fill;
+            DgvTabla2.Location = new System.Drawing.Point(0, 0);
+            DgvTabla2.Name = "DgvTabla2";
+            DgvTabla2.RowHeadersWidth = 30;
+            DgvTabla2.RowTemplate.Height = 28;
+            DgvTabla2.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells; // !!!!!!!!!!
+            DgvTabla2.Size = new System.Drawing.Size(678, 429);
+            DgvTabla2.TabIndex = 0;
+
+            /* ------------------------------------------------------------------------- */
+            // Agrega columnas
+            /* ------------------------------------------------------------------------- */
+            for (int i = 1; i <= numColums; i++)
+            {
+                DgvTabla2.Columns.Add($"C{i}", $"{i}");
+            }
+
+            /* ------------------------------------------------------------------------- */
+            // Agrega las filas
+            /* ------------------------------------------------------------------------- */
+
+            for (int r = 1; r <= numColums; r++)
+            {
+                DgvTabla2.Rows.Add();
+               
+                for (int c = 0; c < numColums; c++)
+                {
+                    DgvTabla2.Rows[r - 1].Cells[c].Value = $"{random.Next() % 10}";
+                }
+            }
         }
 
-        private void panel1_Paint(object sender, PaintEventArgs e)
+        /* ------------------------------------------------------------------------- */
+        // Botón de Calcular tabla Practica 2
+        // - Sumatoria todas las celdas de la matriz
+        // - Sumatoria de todos los pares.
+        /* ------------------------------------------------------------------------- */
+        private void BtnP2Llenar_Click(object sender, EventArgs e)
         {
+            int RowCount = DgvTabla2.RowCount - 1,
+                ColCount = DgvTabla2.ColumnCount,
+                SumaTotal = 0,
+                SumaPar = 0,
+                SumaInpar = 0,
+                CurrentCeilValue,
+                PrevCeilValue =0;
 
-        }
+            bool isWork;
 
-        private void panel1_Paint_1(object sender, PaintEventArgs e)
-        {
+            for ( int i = 0; i < RowCount; i++)
+            {
+                for ( int j = 0; j < ColCount; j++)
+                {
+                    isWork = int.TryParse(DgvTabla2.Rows[i].Cells[j].Value.ToString(), out CurrentCeilValue);
 
-        }
+                    if (!isWork)
+                    {
+                        MessageBox.Show("Hay valores extraños en la matriz");
+                        return;
+                    }
 
-        private void PnlDerecho_Paint(object sender, PaintEventArgs e)
-        {
+                    if (j + 1 < ColCount)
+                    {
+                        isWork = int.TryParse(DgvTabla2.Rows[i].Cells[j + 1].Value.ToString(), out PrevCeilValue);
 
-        }
+                        if (!isWork)
+                        {
+                            MessageBox.Show("Hay valores extraños en la matriz");
+                            return;
+                        }
+                    }
 
-        private void panel1_Paint_2(object sender, PaintEventArgs e)
-        {
+                    SumaTotal += CurrentCeilValue;
 
-        }
+                    if (CurrentCeilValue % 2 == 0)
+                    {
+                        SumaPar += CurrentCeilValue;
+                        DgvTabla2.Rows[i].Cells[j].Style.BackColor = Color.Yellow;
+                    }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
+                    if (CurrentCeilValue % 2 != 0)
+                    {
+                        DgvTabla2.Rows[i].Cells[j].Style.BackColor = Color.LightGreen;
+                    }
 
-        }
+                    if (j + 1 < ColCount && CurrentCeilValue == PrevCeilValue)
+                    {
+                        DgvTabla2.Rows[i].Cells[j].Style.BackColor = Color.Red;
+                        DgvTabla2.Rows[i].Cells[j + 1].Style.BackColor = Color.Red;
+                    }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
+                }
+            }
 
+            TextBoxSalida1.Text = $"{SumaTotal}";
+            TextBoxSalida2.Text = $"{SumaPar}";
+            TextBoxSalida3.Text = $"{SumaInpar}";
+
+            TextBoxSalida2.BackColor = Color.Yellow;
+            TextBoxSalida3.BackColor = Color.LightGreen;
         }
     }
 }
